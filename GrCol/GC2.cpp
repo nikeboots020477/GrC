ead1f6c7b8ac5f459944a3158e9681675c01d700
nikeboots020477 <108978402+nikeboots020477@users.noreply.github.com>
2023-09-19 13:25:32 +0300

    Update GC2.cpp

--- GrCol/GC2.cpp
+++ GrCol/GC2.cpp
@@ -10,18 +10,28 @@
 #include <math.h>
 using namespace std;
 
+struct el
+{int dd;
+int ind;
+};
+
+
 int main(){
      int i,j, k;  
 
 
 
-ifstream data("gc_100_5");
+ifstream data("gc_500_9");
     int N,E; 
     data>>N>>E;
 
     int** mm = new int *[E]; 
     for (int i=0; i < E; i++)
         mm[i] = new int [2];
+    
+    struct el sort[N];
+
+    for(i=0;i<N;i++) {sort[i].dd=0; sort[i].ind=i;}
 
     bool ** gr = new bool *[N]; 
     for (int i=0; i < N; i++)
@@ -29,18 +39,38 @@ ifstream data("gc_100_5");
 
     for(int i = 0; i<E;i++){
         data>>mm[i][0]>>mm[i][1];
-        
+        sort[mm[i][0]].dd= sort[mm[i][0]].dd+1;
+        sort[mm[i][1]].dd= sort[mm[i][1]].dd+1;
     }
-    for(i=0; i<N; i++){
+     
+struct el temp;
+for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (sort[j].dd < sort[j + 1].dd) {
+                temp = sort[j];
+                sort[j]= sort[j + 1];
+                sort[j + 1] = temp;
+            }
+        }
+    }
+
+for(i=0; i<N; i++){
         for(j=0; j<N; j++)
             gr[i][j] = 0;
         gr[i][i]=1;
     }
-    for(i=0; i<E; i++){
+int temp2[E][2];
+for(i=0;i<E;i++)  {mm[i][0]= sort[mm[i][0]].ind;
+mm[i][1]= sort[mm[i][1]].ind;
+}
+
+for(i=0; i<E; i++){
         gr[mm[i][0]][mm[i][1]]=true;
         gr[mm[i][1]][mm[i][0]]=true;
     }
-    data.close();
+
+
+data.close();
 
     int cl[N];
     for(i=0; i<N; i++) cl[i]=0;
